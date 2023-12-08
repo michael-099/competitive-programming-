7d66e783af4fb1437375c02ebf5573f6ada7ee1d
michael shimeles <105845333+michael-099@users.noreply.github.com>
2023-12-08 11:19:58 +0300

    Added solution for Construct String from Binary Tree

---
+++ FirstPhase/week2/construct-string-from-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        pre=[]
+        
+        def preorder(root):
+            if not root :
+                return
+            pre.append("(")
+            pre.append(str(root.val))
+            if not root.left and root.right:
+                pre.append("()")
+            preorder(root.left)
+            preorder(root.right)
+            pre.append(")")
+
+        preorder(root)
+        return "".join(pre)[1:-1]
+            
+        
+
+
+     
+        
+        
