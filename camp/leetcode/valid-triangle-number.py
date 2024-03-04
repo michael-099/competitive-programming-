74fe10af2c10a10a01455979d6f13e3f5dbb9d29
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-03-04 08:41:35 +0300

    Add solution for Valid Triangle Number

--- camp/leetcode/valid-triangle-number.py
+++ camp/leetcode/valid-triangle-number.py
@@ -1,5 +1,10 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        """we will initialize 3 pointers  the left pointer at index 0  the right pointer at index 1 
+         and the i pointer at index 2 then as the ith pointer move one index also the right pointer will move
+         one step and the left pointer will going to increment while the sum of nums[left] and nums[right] less than
+          the nums[i] when the opposite is true we will chake every less subbarray by moving the right pointer till left+1 
+          and we will add left-right to the answer variable and finaly we will going to return the answer variable"""
         nsot = 0
         nums.sort()
 
@@ -9,6 +14,8 @@ class Solution:
             while left < right:
                 if nums[left] + nums[right] > nums[i]:
                     nsot += right - left
+                    # print(f"left={left} right={right} i={i}")
+                    # print(nsot)
                     right -= 1
                 else:
                     left += 1
