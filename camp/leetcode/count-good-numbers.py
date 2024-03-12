3d2f59a50da582b5d636268965cc496ae84d22bc
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-03-12 11:35:37 +0300

    Add solution for Count Good Numbers

--- camp/leetcode/count-good-numbers.py
+++ camp/leetcode/count-good-numbers.py
@@ -3,15 +3,14 @@ class Solution:
         mod = (10**9) + 7
         odd = n // 2
         even = n // 2 + n % 2
+        # in the last two lines we are trying to find how many odd places and even places are there
         return (self.binary(5, even) % mod * self.binary(4, odd) % mod) % mod
 
     def binary(self ,val, n):
         mod = (10**9) + 7
         if n == 0:
             return 1
-        if n<0:
-            return 1/self.binary(val,-n)
-
+        
         if n % 2 == 0:
             return self.binary((val * val) % mod, (n // 2))
         else:
