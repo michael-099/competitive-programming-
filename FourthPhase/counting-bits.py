657b98d8a28b6666323cb25830091d612cd8d54a
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-04-30 18:18:11 +0300

    Added solution for Counting Bits

--- FourthPhase/counting-bits.py
+++ FourthPhase/counting-bits.py
@@ -3,14 +3,14 @@ class Solution:
 
         def count_ones(n,count):
             while n!=0:
-                rem=n%2
-                if rem==1:
+                if n & 1:
                     count+=1
-                n=n//2
+                n=n>>1
             return count
 
         outPut=[]
         for i in range(n+1):
             outPut.append(count_ones(i,0))
+            
         return outPut
         
