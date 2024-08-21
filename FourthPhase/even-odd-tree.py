13976b3eb57a209f6e6bec1a592e74440cad6cfd
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-21 16:10:32 +0300

    Added solution for Even Odd Tree

--- FourthPhase/even-odd-tree.py
+++ FourthPhase/even-odd-tree.py
@@ -1,51 +1,33 @@
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        def is_increasing(arr):
-            if len(arr)==1:
-                if arr[0] % 2 == 0 :
-                    return False
-            for i in range(len(arr) - 1):
-                if arr[i] >= arr[i + 1]:
-                    return False
-                if arr[i] % 2 == 0 or arr[i+1] % 2 == 0:
-                    return False
-            return True
-
-        def is_decreasing(arr):
-            if len(arr)==1:
-                if arr[0] % 2 != 0 :
-                    return False
-            for i in range(len(arr) - 1):
-                if arr[i] <= arr[i + 1] :
-                    return False
-                if arr[i] % 2 != 0 or arr[i+1] % 2 != 0:
-                    return False
-            return True
-
+        def is_even(num):return num%2==0
         queue = deque()
         queue.append(root)
-        arr = []
+        current,level=None,0
+        
         while queue:
-            temp = []
+            prev=None
             for i in range(len(queue)):
-                temp.append(queue[i].val)
-            arr.append(temp)
-
-            for i in range(len(queue)):
-                x = queue.popleft()
-                if x.left:
-                    queue.append(x.left)
-                if x.right:
-                    queue.append(x.right)
-        print(arr)
-        for i in range(len(arr)):
-            if i % 2 == 0:
-                print(is_increasing(arr[i]))
-                if not is_increasing(arr[i]):
-                    return False
-            else:
-                print(is_decreasing(arr[i]))
-                if not is_decreasing(arr[i]):
-                    return False
-
-        return True
+                current = queue.popleft()
+                if current :
+                    if is_even(level):
+                        if is_even(current.val):
+                            return False
+                        if prev is not None :
+                            if current.val<= prev:
+                                return False
+                    else:
+                        if not is_even(current.val) :
+                            return False
+                        if prev is not None:
+                            if current.val>=prev:
+                                return False
+                    prev=current.val
+                    if current.left:
+                        queue.append(current.left)
+                    if current.right:
+                        queue.append(current.right)
+            
+            level+=1
+        return True 
+        
